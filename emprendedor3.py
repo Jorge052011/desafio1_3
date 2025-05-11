51b1673ee3f9d2dbca1b0cfd34dcea4018df3721
Jorge052011 <jimunozacuna+1@gmail.com>
2025-05-11 18:13:13 +0000

    se ingresa sentencia if para descartar división por cero

--- emprendedor3.py
+++ emprendedor3.py
@@ -9,7 +9,13 @@ Usuarios_normales = int(input("ingrese número de usuarios normales : "))
 Gastos = float(input("ingrese gastos totales: "))
 Utilidad_anterior = float(input("ingrese las utilidades del año anterior en pesos, para el cálculo de la razón este debe ser diferente de cero: "))
 
-Utilidades_actuales = (Precio_Suscripcion * Usuarios_normales)-Gastos
-Razon = Utilidades_actuales / Utilidad_anterior
+if Utilidad_anterior == 0:
+    print("no se puede calcular la razón ya que ingresó el valor cero en la utilidades anteriores")
 
-print(f"La utilidad actual es de  {Utilidades_actuales:.2f} pesos y la razón entre la utilidad actual y la utilidad anterior es de {Razon:.2f}")
+else:
+
+
+    Utilidades_actuales = (Precio_Suscripcion * Usuarios_normales)-Gastos
+    Razon = Utilidades_actuales / Utilidad_anterior
+
+    print(f"La utilidad actual es de  {Utilidades_actuales:.2f} pesos y la razón entre la utilidad actual y la utilidad anterior es de {Razon:.2f}")
